eea8623af61d5b9fa3db542bc8a223147262ba69
Clay Stuart <clay.stuart@gmail.com>
2017-06-07 13:32:34 -0400

    hilo version 1

--- hiLoServer.go
+++ hiLoServer.go
@@ -1,20 +1,41 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var answer = 25
+var guesses = 0
 
 func main() {
+	http.HandleFunc("/hilo/", hilo)
 	http.HandleFunc("/", index)
-	http.HandleFunc("/route/", route)
 	fmt.Println("Server listening on port http://localhost:5000/")
 	http.ListenAndServe("localhost:5000", nil)
 }
-func route(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from route</h1>")
+func hilo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Guess a number between 1 and 100 using hilo/[number]</h1>")
 	fmt.Fprintf(w, "<h1>Path: %s</h1>", r.URL.Path)
-	fmt.Fprintf(w, "<h1>Type: %T</h1>", r.URL.Path)
+	guess := r.URL.Path[len("/hilo/"):]
+	fmt.Fprintf(w, "<h1>Type: %T = Value: %v</h1>", guess, guess)
+
+	// convert guess from string to int
+	s, err := strconv.Atoi(guess)
+	if err != nil {
+		fmt.Fprint(w, "<h1>Error detected in String conversion of guess</h1>")
+	}
+	if s < answer {
+		fmt.Fprintf(w, "<h1>Too Low</h1>")
+	} else if s > answer {
+		fmt.Fprintf(w, "<h1>Too high</h1>")
+	} else {
+		fmt.Fprintf(w, "<h1>Correct!</h1><h1>You win!!!</h1>")
+	}
+
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from index</h1>")
+	fmt.Fprintf(w, "<h1>Guess a number between 1 and 100 using hilo/[number]</h1>")
 }
